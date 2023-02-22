d3a7f94c34aa9508fa2f6ffcf8c94e9eb87d4b54
MSKehribar <msk.kehribar@gmail.com>
2023-02-22 22:13:04 +0300

    Json Çalışması 1

---
+++ BTK_json_uygulamasi.py
@@ -0,0 +1,68 @@
+import os                           #Terminal temizlemek için
+os.system('cls||clear')
+
+
+class User:
+    pass
+
+class UserRepo:
+    def reg(self):
+        pass
+
+    def login(self):
+        pass
+
+    def savetofile(self):
+        pass
+
+while True:
+    print('Menü'.center(50,'*'))
+    secim=input('1_-Register\n2-Login\n3-Logout\n4-Identity\nPress another key for EXİT')
+
+    if secim==1:
+        pass
+    elif secim==2:
+        pass
+    elif secim==3:
+        pass
+    elif secim==4:
+        pass
+    else:
+        break #Exit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
